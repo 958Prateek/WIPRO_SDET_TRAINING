c33c725a5ab0b67b43e4e81f2dd7f28dd4244163
958Prateek <chaturvediprateek958@gmail.com>
2026-05-05 12:50:38 +0530

    promises , afetching data from json data

--- Day-4/index.js
+++ Day-4/index.js
@@ -1,22 +1,66 @@
+// function getData() {
+//     return new Promise((resolve, reject) => {
+
+//         setTimeout(() => {
+//             resolve("Data received");
+//         }, 2000);
+
+//     });
+// }
+
+// getData()
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.error("Error:", error);
+//     });
+
+// // setTimeout example
+// setTimeout(() => {
+//     console.log("Data received after 5 seconds");
+// }, 5000);
+
+// setInterval example
+// setInterval(() => {
+//     console.log("Checking for new data...");
+// }, 3000);
+
 function getData() {
     return new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-            resolve("Data received");
-        }, 2000);
-
+        if (2 > 3) {
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
+        } else {
+            reject("not working");
+        }
     });
 }
 
-getData()
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+//Fetch API Chain
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(res => res.json()) //covert response to json
+.then(data => data.filter(d => d.id % 2 === 0)) // filter even ids
+.then(data => data.map(d =>(d.title = "Prateek" + d.title))) // mpdify
+.then(data => console.log(data)) //print result
+.catch(error => console.error("Error fetching data :", error));
 
 
+// //Calling getData()
+
+// getData() 
+//      .then(data => {
+//           console.log("getData success :", data);
+//      })
+//      .catch(error => {
+//           console.error("getData error:" , error);
+//      });
+
+
+
+// // synchronous log
+// console.log("test");
 
 
 
