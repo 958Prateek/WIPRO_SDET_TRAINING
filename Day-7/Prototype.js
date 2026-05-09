0668b0952cd8fd8e81cb48fd11ea0bb17ab70ee4
958Prateek <chaturvediprateek958@gmail.com>
2026-05-09 10:35:11 +0530

    Prototype Examape

--- Day-7/Prototype.js
+++ Day-7/Prototype.js
@@ -5,3 +5,30 @@ console.log(rabbit.eats); // inherted from animal
 console.log(rabbit.hasOwnProperty('eats'))
 console.log(rabbit.hasOwnProperty('carrot'))
 console.log(rabbit.toString())
+
+
+const person = {
+    greet: function() {
+        console.log("Hello!");
+    }
+};
+const student = Object.create(person);
+student.name = "Prateek";
+console.log(student.name);
+student.greet();
+
+
+
+const vehicle = {
+    start: function() {
+        console.log("Vehicle started");
+    }
+};
+const car = Object.create(vehicle);
+car.drive = function() {
+    console.log("Car is driving");
+};
+car.start();
+car.drive();
+console.log(car.hasOwnProperty("drive"));
+console.log(car.hasOwnProperty("start"));
