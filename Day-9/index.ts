1280100543466beba595ba2d036c0fd006cb78a6
958Prateek <chaturvediprateek958@gmail.com>
2026-05-11 14:09:51 +0530

    Restored full project structure

--- Day-9/index.ts
+++ Day-9/index.ts
@@ -1,2 +1,12 @@
 let name = "Prateek";
-console.log(name);
+let result: number | string;
+
+result = 45;
+result = "success";
+
+console.log(name , result);
+
+function add(a: number, b: number) : string {
+     return"Sum: " + (a + b);
+}
+console.log(add(5,10));
