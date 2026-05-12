e102236a303b1cbc316868d3128fe2781da9a62b
958Prateek <chaturvediprateek958@gmail.com>
2026-05-12 15:43:36 +0530

    Removed node_modules and added gitignore

--- axios-demo/app.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const URL = "http://api.open-notify.org/astros.json";
-
-axios
-  .get(URL)
-  .then((res) => console.log(res.data))
-  .catch((err) => console.log(err));
